37973366a7128b1afe0e075508585ede007f7e93
nestora103 <nestora103@gmail.com>
2016-01-03 15:32:11 +0300

    ООП подход.

--- snake/snake/Point.cs
+++ snake/snake/Point.cs
@@ -12,7 +12,7 @@ namespace snake
         public int y;
         public char sym;
 
-        public void Draw( int x, int y, char sym)
+        public void Draw()
         {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -14,13 +14,13 @@ namespace snake
             p1.x = 1;
             p1.y = 3;
             p1.sym = '*';
-            p1.Draw(p1.x,p1.y,p1.sym);
+            p1.Draw();
 
             Point p2 = new Point();
-            p2.x = 2;
+            p2.x = 5;
             p2.y = 5;
             p2.sym = '#';
-            p2.Draw(p2.x, p2.y, p2.sym);
+            p2.Draw();
         }
         
     }
