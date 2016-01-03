ea608f6df80903f80251f0937fc31a7a5272b52a
nestora103 <nestora103@gmail.com>
2016-01-03 21:18:27 +0300

    Попытка сделать получить разрешение экрана. Получилось. Но развернуть консоль на весь экран нельзя. Добавлен размер консоли 80 на 25. Отрисована рамка. Применено наследование, добавлен класс Figure наследниками которого будут HorizontalLine и VerticalLine.

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -3,42 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Runtime.InteropServices;
 namespace snake
 {
     class Program
     {
+        /*[DllImport("user32.dll")]
+        public static extern int GetSystemMetrics(int nIndex);
+        public const int SM_CXSCREEN = 0;
+        public const int SM_CYSCREEN = 1;*/
+
         static void Main(string[] args)
-        {
-            //Создаем список, который будет хранить элементы типа точки
-            List<Point> pList = new List<Point>();
-            //Создаем точки для вывода 
-            Point p1 = new Point( 1, 3, '*');
-            Point p2 = new Point( 1, 5, '#');
-            Point p3 = new Point( 1, 7, '$');
-            Point p4 = new Point( 1, 9, '%');
-            Point p5 = new Point(1, 11, '&');
-            //Добавляем точки в список
-            pList.Add(p1);
-            pList.Add(p2);
-            pList.Add(p3);
-            pList.Add(p4);
-            pList.Add(p5);
-            //Выводим поэлементно из списка
-            foreach (Point p in pList)
-            {
-                p.Draw();
-            }
+        {   
+            
+            /*int screenW = GetSystemMetrics(SM_CXSCREEN);
+            int screenH = GetSystemMetrics(SM_CYSCREEN);*/
 
-            //рисуем горизонтальную линию.
-            HorizontalLine lineH = new HorizontalLine(7,15,10,'£');
-            lineH.DrowHline();
+            Console.SetBufferSize(80,25);
+            
+            //Отрисуем рамку
+            HorizontalLine lineH1 = new HorizontalLine(1,78,0,'#');
+            lineH1.Draw();
+            HorizontalLine lineH2 = new HorizontalLine(1, 78, 24, '#');
+            lineH2.Draw();
+            VerticalLine lineV1 = new VerticalLine(1,0,24,'#');
+            lineV1.Draw();
+            VerticalLine lineV2 = new VerticalLine(78, 0, 24,'#');
+            lineV2.Draw();
 
-            //рисуем вертикальную линию.
-            VerticalLine lineV = new VerticalLine(15,1,10,'£');
-            lineV.DrawVline();
-
-            Console.ReadLine();          
+            Console.ReadLine();     
+            
+                 
         }
 
     }
