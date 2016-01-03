606d46c87301c2d98cda9aba9b086970b715b2b0
nestora103 <nestora103@gmail.com>
2016-01-03 18:43:24 +0300

    Добавили класс для рисования вертикальной линии

---
+++ snake/snake/VerticalLine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class VerticalLine
+    {
+        List<Point> pLine = new List<Point>();
+
+        public VerticalLine(int x, int yTop, int yBottom, char sym)
+        {
+            for(int y= yTop; y <= yBottom; y++)
+            {
+                Point point = new Point(x,y,sym);
+                pLine.Add(point);
+            }
+        }
+
+        public void DrawVline()
+        {
+            foreach(Point p in pLine)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
