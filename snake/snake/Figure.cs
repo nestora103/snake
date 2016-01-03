b5f2368a4833105996fce4d3502237f01b82ff10
nestora103 <nestora103@gmail.com>
2016-01-03 21:20:06 +0300

    Добавлени класса Figure

---
+++ snake/snake/Figure.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class Figure
+    {
+       public List<Point> pLine = new List<Point>();
+
+        public void Draw()
+        {
+            foreach (Point p in pLine)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
