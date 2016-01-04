523a0f403697bd46fc8bf89f3a27e60f0ad1c665
nestora103 <nestora103@gmail.com>
2016-01-04 13:22:13 +0300

    Добавлен класс змейка, который наследован от фигуры, т.е его можно будет отрисовать. У змейки есть хвост, длинна и направление в котором она будет отрисовываться. Все отсчеты идут от хвостовой точки.

---
+++ snake/snake/Snake.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    //Змейка это фигура, т.е список точек
+    class Snake:Figure
+    {
+        //параметры хвост тоесть точка, размер змейки, и направление в которое она перемещается
+        public Snake(Point tail,int length,Diraction diraction)
+        {
+            //заполнение списка точек змейки
+            for (int i=0;i<length;i++)
+            {
+                //на основе хвостовой точки будем получать ост. точки змейки
+                Point p = new Point(tail);
+                //каждую точку будем получать сдвигом от хвоста
+                p.Move(i, diraction);
+                //получ. точку в список.
+                pLine.Add(p);
+            }
+        }
+    }
+}
